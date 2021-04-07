f5d0e26d1361061d544025530c2a33a3df430e08
Man Ho <manhm@rch-asia.com>
2021-04-07 16:36:05 +0700

    Nav view

--- app/src/main/kotlin/com/homanad/android/w2/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/homanad/android/w2/ui/main/MainActivity.kt
@@ -3,12 +3,13 @@ package com.homanad.android.w2.ui.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.viewbinding.ViewBinding
+import com.homanad.android.common.components.ui.BaseActivity
 import com.homanad.android.common.components.ui.BaseActivityWithBinding
-import com.homanad.android.common.components.ui.binding.contentView
+import com.homanad.android.common.components.ui.binding.ContentViewBindingDelegate
 import com.homanad.android.w2.R
 import com.homanad.android.w2.databinding.ActivityMainBinding
 import com.homanad.android.w2.ui.main.viewmodel.MainViewModel
@@ -16,24 +17,28 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import np.com.susanthapa.curved_bottom_navigation.CbnMenuItem
 
 @ExperimentalCoroutinesApi
-class MainActivity : BaseActivityWithBinding() {
+class MainActivity : AppCompatActivity() {
 
     private val mainViewModel: MainViewModel by viewModels()
-
-
-    override val binding: ActivityMainBinding by contentView(R.layout.activity_main)
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        observeData()
+        updateUI()
+    }
+
+    private fun observeData() {
 
     }
 
-    override fun observeData() {
-
+    private fun updateUI() {
+        setupBottomNav()
     }
 
-    override fun updateUI() {
+    private fun setupBottomNav() {
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
@@ -44,31 +49,29 @@ class MainActivity : BaseActivityWithBinding() {
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-//        val menuItems = arrayOf(
-//            CbnMenuItem(
-//                R.drawable.ic_notification,
-//                R.drawable.avd_notification,
-//                R.id.navigation_notifications
-//            ),
-//            CbnMenuItem(
-//                R.drawable.ic_dashboard,
-//                R.drawable.avd_dashboard,
-//                R.id.navigation_dashboard
-//            ),
-//            CbnMenuItem(R.drawable.ic_home, R.drawable.avd_home, R.id.navigation_home),
-//            CbnMenuItem(
-//                R.drawable.ic_profile,
-//                R.drawable.avd_profile,
-//                R.id.navigation_profile
-//            ),
-//            CbnMenuItem(
-//                R.drawable.ic_settings,
-//                R.drawable.avd_settings,
-//                R.id.navigation_settings
-//            )
-//        )
-//        binding.navView.setMenuItems(menuItems, 0)
-//        binding.navView.setupWithNavController(navController)
+        val menuItems = arrayOf(
+            CbnMenuItem(
+                R.drawable.ic_home,
+                R.drawable.avd_home,
+                R.id.homeFragment
+            ),
+            CbnMenuItem(
+                R.drawable.ic_account,
+                R.drawable.avd_account,
+                R.id.accountFragment
+            ),
+            CbnMenuItem(
+                R.drawable.ic_statistics,
+                R.drawable.avd_statistics,
+                R.id.statisticFragment
+            ),
+            CbnMenuItem(
+                R.drawable.ic_settings,
+                R.drawable.avd_settings,
+                R.id.settingFragment
+            )
+        )
+        binding.navView.setMenuItems(menuItems, 0)
+        binding.navView.setupWithNavController(navController)
     }
 }
