3f551e720808051f920459bd68f9f58d328e2ef2
Man Ho <manhm@rch-asia.com>
2021-04-19 16:59:53 +0700

    Add log testing

--- app/src/main/kotlin/com/homalab/android/w2/ui/pages/log/LogFragment.kt
+++ app/src/main/kotlin/com/homalab/android/w2/ui/pages/log/LogFragment.kt
@@ -4,10 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.homalab.android.w2.R
+import com.homalab.android.w2.data.model.Expense
+import com.homalab.android.w2.databinding.FragmentLogBinding
+import com.homalab.android.w2.ui.main.intent.MainIntent
+import com.homalab.android.w2.ui.main.viewmodel.MainViewModel
 import com.homanad.android.common.components.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
+@ExperimentalCoroutinesApi
 class LogFragment : BaseFragment() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var binding: FragmentLogBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +38,41 @@ class LogFragment : BaseFragment() {
     }
 
     override fun updateUI() {
-
+        with(binding) {
+            buttonSave.setOnClickListener {
+                test()
+                findNavController().navigateUp()
+            }
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_log, container, false)
+        return binding.root
+    }
+
+    //TODO testing
+    private fun test() {
+        lifecycleScope.launch {
+            mainViewModel.userIntent.send(
+                MainIntent.CreateExpenseIntent(
+                    Expense(
+                        0,
+                        "b",
+                        "b",
+                        1111f,
+                        1,
+                        System.currentTimeMillis(),
+                        System.currentTimeMillis()
+                    )
+                )
+            )
+
+            mainViewModel.userIntent.send(MainIntent.GetAllExpensesIntent)
+        }
     }
 
     companion object {
