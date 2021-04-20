c9a5dd7cd35d26ab8df164eda8675d1f22fd46e7
Man Ho <manhm@rch-asia.com>
2021-04-20 10:57:22 +0700

    Navigate to Category

--- app/src/main/kotlin/com/homalab/android/w2/ui/pages/setting/SettingFragment.kt
+++ app/src/main/kotlin/com/homalab/android/w2/ui/pages/setting/SettingFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.homalab.android.w2.R
 import com.homalab.android.w2.databinding.FragmentSettingBinding
 import com.homalab.android.w2.ui.main.viewmodel.MainViewModel
 import com.homalab.android.w2.ui.pages.setting.adapter.SettingAdapter
+import com.homalab.android.w2.ui.pages.setting.model.SettingType
 import com.homanad.android.common.components.recyclerView.decoration.SpaceItemDecoration
 import com.homanad.android.common.components.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,7 +28,11 @@ class SettingFragment : BaseFragment() {
 
     private val settingAdapter by lazy {
         SettingAdapter { view, settingType ->
-
+            when (settingType) {
+                SettingType.CATEGORY -> {
+                    navigateTo(SettingFragmentDirections.actionSettingFragmentToCategoryFragment())
+                }
+            }
         }
     }
 
@@ -57,6 +64,10 @@ class SettingFragment : BaseFragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_setting, container, false)
         return binding.root
+    }
+
+    private fun navigateTo(direction: NavDirections) {
+        findNavController().navigate(direction)
     }
 
     companion object {
---
+++ app/src/main/kotlin/com/homalab/android/w2/ui/pages/setting/features/category/CategoryFragment.kt
@@ -0,0 +1,53 @@
+package com.homalab.android.w2.ui.pages.setting.features.category
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.homalab.android.w2.R
+import com.homalab.android.w2.databinding.FragmentCategoryBinding
+import com.homalab.android.w2.ui.main.viewmodel.MainViewModel
+import com.homanad.android.common.components.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@AndroidEntryPoint
+@ExperimentalCoroutinesApi
+class CategoryFragment : BaseFragment() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var binding: FragmentCategoryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun observeData() {
+
+    }
+
+    override fun setupViewModel() {
+
+    }
+
+    override fun updateUI() {
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_category, container, false)
+        return binding.root
+    }
+
+    companion object {
+        private val TAG = CategoryFragment::class.java.simpleName
+
+        @JvmStatic
+        fun newInstance() = CategoryFragment()
+    }
+}
