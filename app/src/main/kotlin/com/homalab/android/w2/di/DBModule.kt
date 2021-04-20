371d25600aa8aaac6071c76fe9090769b2555f3a
Man Ho <manhm@rch-asia.com>
2021-04-20 13:59:52 +0700

    Pre-populating expense-categories

--- app/src/main/kotlin/com/homalab/android/w2/di/DBModule.kt
+++ app/src/main/kotlin/com/homalab/android/w2/di/DBModule.kt
@@ -2,23 +2,40 @@ package com.homalab.android.w2.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.homalab.android.w2.data.db.WDatabase
+import com.homalab.android.w2.data.db.categories
+import com.homalab.android.w2.data.db.dao.CategoryDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Provider
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DBModule {
 
     @Provides
-    fun provideWDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+    fun provideWDatabase(@ApplicationContext context: Context, categoryDaoProvider: Provider<CategoryDao>) = Room.databaseBuilder(
         context,
         WDatabase::class.java,
         WDatabase::class.java.simpleName
-    ).build()
+    )
+        .addCallback(object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                CoroutineScope(Dispatchers.IO).launch {
+                    categoryDaoProvider.get().insert(categories)
+                }
+            }
+        })
+        .build()
 
     @Provides
     fun provideExpenseDao(wDatabase: WDatabase) = wDatabase.expenseDao
