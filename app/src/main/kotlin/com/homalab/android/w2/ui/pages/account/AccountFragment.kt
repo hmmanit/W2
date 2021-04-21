f546c6b8ac6de7fadf21825c76603de94c92fae6
Man Ho <manhm@rch-asia.com>
2021-04-21 14:01:32 +0700

    Account UI

--- app/src/main/kotlin/com/homalab/android/w2/ui/pages/account/AccountFragment.kt
+++ app/src/main/kotlin/com/homalab/android/w2/ui/pages/account/AccountFragment.kt
@@ -1,26 +1,46 @@
 package com.homalab.android.w2.ui.pages.account
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.homalab.android.w2.R
+import com.homalab.android.w2.databinding.FragmentAccountBinding
+import com.homanad.android.common.components.ui.BaseFragment
 
-class AccountFragment : Fragment() {
+class AccountFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentAccountBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    override fun observeData() {
+
+    }
+
+    override fun setupViewModel() {
+
+    }
+
+    override fun updateUI() {
+
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_account, container, false)
+        return binding.root
     }
 
     companion object {
+        private val TAG = AccountFragment::class.java.simpleName
+
         @JvmStatic
         fun newInstance() = AccountFragment()
     }
